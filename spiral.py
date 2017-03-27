1f73afe55b7ac68f4da24287e8f528ee1f827867
Serge Stinckwich <Serge.Stinckwich@gmail.com>
2017-03-27 11:30:15 +0200

    Add Spirale arms

--- spiral.py
+++ spiral.py
@@ -8,12 +8,14 @@ fig.set_size_inches(7, 6.5)
 
 ax = plt.axes(xlim=(-20, 20), ylim=(-20, 20))
 
-size = 0.2
+size = 0.3
 xcenter = 0
 ycenter = 0
 radius = 1
 
 n = 10
+
+j = 10
 
 patch_list = []
 for i in range(n):
@@ -27,17 +29,24 @@ def init():
 
 def animate(t):
     i = 1
+#    xj = xcenter + j*radius * np.sin(np.radians(20*t/j))
+#    yj = ycenter + j*radius * np.cos(np.radians(20*t/j))
+
+    xj = 0
+    yj = 0
     for p in patch_list:
         x, y = p.center
-        x = xcenter + i*radius * np.sin(np.radians(t*i/5))
-        y = ycenter + i*radius * np.cos(np.radians(t*i/5))
+        r = i*radius
+        x = -xj + xcenter + r * np.sin(np.radians(t/i))
+        y = -yj + ycenter + r * np.cos(np.radians(t/i))
+        # plt.plot(x,y,'o')
         p.center = (x, y)
         i=i+1
     return patch_list
 
 anim = animation.FuncAnimation(fig, animate, 
                                init_func=init, 
-                               frames=360, 
+                               frames=7200, 
                                interval=20,
                                blit=True)
 plt.show()
